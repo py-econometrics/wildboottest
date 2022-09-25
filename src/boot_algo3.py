06d97220bff83873e3003dff323cfb34f6e823a8
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-09-25 22:24:54 +0200

    fix worst syntax errors

--- src/boot_algo3.py
+++ src/boot_algo3.py
@@ -1,19 +1,21 @@
-def boot_algo3(X, y, bootstrap_type, N_G_bootcluster, R): 
+import numpy as np
+
+def boot_algo3(X, y, bootstrap_type, N_G_bootcluster, R, impose_null): 
   
   assert bootstrap_type in ['11', '13', '31', '33']
   assert if isinstance(X, np.ndarray)
   assert if isinstance(y, np.ndarray)
 
-  if( bootstrap_type[0:1] == 1):
-    crv_type = "crv1
-  elif: 
-    crv_type = "crv3
+  if(bootstrap_type[0:1] == '1'):
+    crv_type = "crv1"
+  elif(bootstrap_type[0:1] == '3'): 
+    crv_type = "crv3"
     
   bootstrap_type_x = bootstrap_type[1:2] + 'x'
   
   if(impose_null == True):
     bootstrap_type = "WCR" + bootstrap_type_x
-  elif: 
+  else: 
     bootstrap_type = "WCU" + bootstrap_type_x
 
   # write dedicated get_weights() function
@@ -21,20 +23,22 @@ def boot_algo3(X, y, bootstrap_type, N_G_bootcluster, R):
   
   X_list = []
   y_list = []
-  XgXg = []
-  Xgyg = []
+  XgXg_list = []
+  Xgyg_list = []
   tXX = np.zeros((k, k))
   tXy = np.zeros((N,1))
   
-  for g in range(0, N_G_bootcluster):
-    X_g = X[np.where(bootcluster == g), ]
-    Y_g = y[np.where(bootcluster == g), ]
+  for g in np.unique(bootcluster):
+    
+    X_g = X[bootcluster == g, :]
+    Y_g = y[bootcluster == g, :]
+    tXgXg = np.matmul(np.transpose(X_g), X_g)
+    tXgyg = np.matmul(np.transpose(X_g), Y_g)
+    
     X_list.append(X_g)
     y_list.append(y_g)
-    XgXg = np.matmul(Xg)
-    tXgyg = np.matmul(np.transpose(Xg), yg)
-    XgXg.append(XgXg))
-    Xgyg.append(tXgyg)
+    XgXg_list.append(tXgXg)
+    Xgyg_list.append(tXgyg)
     tXX = tXX + XgXg
     tXy = tXy + tXgyg
     
@@ -53,7 +57,7 @@ def boot_algo3(X, y, bootstrap_type, N_G_bootcluster, R):
   
     if(bootstrap_type in ["WCR3x", "WCU3x"]: 
       
-      X1 = X[,np.where(R == 0)]
+      X1 = X[:,np.where(R == 0)]
       X1_list = []
       tX1gX1g = []
       tX1gyg = []
@@ -61,13 +65,13 @@ def boot_algo3(X, y, bootstrap_type, N_G_bootcluster, R):
       tX1X1 = np.array()
       tX1y = np.array()
       
-      for x in range(0, N_G):
-        X1_list.append(X1[np.where(bootcluster == x)])
-        tX1gX1g.append(np.matmul(X1_list[x]))
-        tX1gyg.append(np.matmul(np.transpose(X1_list[x], y_list[x])))
-        tXgX1g.append(np.transpose(X_list[x]), X1_list[x])
-        tX1X1 = tX1X1 + tX1gX1g[x]
-        tX1y = tX1y + tX1gyg[x]
+    for g in np.unique(bootcluster):
+        X1_list.append(X1[bootcluster == g,:])
+        # tX1gX1g.append(np.matmul(np.transpose(X1_list[g]), X1_list[g])
+        # tX1gyg.append(np.matmul(np.transpose(X1_list[x], y_list[x])))
+        # tXgX1g.append(np.transpose(X_list[x]), X1_list[x])
+        # tX1X1 = tX1X1 + tX1gX1g[x]
+        # tX1y = tX1y + tX1gyg[x]
         
       tX1X1inv = np.invert(tX1X1)
   
