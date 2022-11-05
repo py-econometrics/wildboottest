c1ade3c79cccdbe405223021e34c4f95dd7203f6
Alexander Fischer <alexander-fischer1801@t-online.de>
2022-11-05 12:54:00 +0100

    add tests for x3 types

--- tests/test_bootalgo.py
+++ tests/test_bootalgo.py
@@ -75,7 +75,7 @@ def test_r_vs_py_deterministic():
   boot_tstats = []
   fwildclusterboot_boot_tstats = []
   
-  for bootstrap_type in ['11', '31']: 
+  for bootstrap_type in ['11', '31', '31', '33']: 
     for impose_null in [True, False]:
       # python implementation
       boot = Wildboottest(X = X, Y = Y, cluster = cluster, bootcluster = cluster, R = R, B = B, seed = 12341)
@@ -152,7 +152,7 @@ def test_r_vs_py_stochastic():
   boot_pvals = []
   fwildclusterboot_boot_pvals = []
 
-  for bootstrap_type in ['11', '31']: 
+  for bootstrap_type in ['11', '31', '31', '33']: 
     for impose_null in [True, False]:
       for weights_type in ['rademacher','mammen', 'webb','norm']:
         for pval_type in ['two-tailed', 'equal-tailed', '>', '<']:
