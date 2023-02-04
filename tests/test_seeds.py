7a897d78034d1c143ce4c932f7943b07d18a4ff4
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-02-04 16:26:37 +0100

    add more tests for seeding - heteroskedastic and global seeds

--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -27,9 +27,25 @@ def test_results_from_same_seed():
     df = pd.read_csv("https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/sandwich/PetersenCL.csv")
     df['treat'] = np.random.choice([0, 1], df.shape[0], True)
     model = sm.ols(formula='y ~ treat', data=df)    
-    
-    for i in range(100):
-        a = wildboottest(model, param = "treat", cluster = df.year, B= 999, seed=11232198237961)
-        b = wildboottest(model, param = "treat", cluster = df.year, B= 999, seed=11232198237961)
+
+    cluster_list = [df.years, None]:   
+    for x in cluster_list: 
+        
+        # same seed used in function -> same results
+        a = wildboottest(model, param = "treat", cluster = x, B= 999, seed=11232198237961)
+        b = wildboottest(model, param = "treat", cluster = x, B= 999, seed=11232198237961)
         pd.testing.assert_frame_equal(a,b)
-    
+
+        # random seed set outside of function and in function produce equal results
+        np.random.default_rng(123)
+        a1 = wildboottest(model, param = "treat", cluster = x, B= 999)
+        b1 = wildboottest(model, param = "treat", cluster = x, B= 999, seed=123)
+        pd.testing.assert_frame_equal(a1,b1)
+        
+        # random seed outside of function 2x -> same results
+        np.random.default_rng(123)
+        a2 = wildboottest(model, param = "treat", cluster = x, B= 999)
+        np.random.default_rng(123)
+        b2 = wildboottest(model, param = "treat", cluster = x, B= 999)
+        pd.testing.assert_frame_equal(a2,b2)
+
