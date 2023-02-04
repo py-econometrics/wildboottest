dd4504f0b1eefe7d5e018b5e17874d9830436894
Alexander Fischer <alexander-fischer1801@t-online.de>
2023-02-04 16:36:16 +0100

    drop dependency on the www, as dangerous in unit tests(but maybe me being overly afraid of cran checks)

--- tests/test_seeds.py
+++ tests/test_seeds.py
@@ -5,47 +5,49 @@ from wildboottest.wildboottest import wildboottest
 import numpy as np
 
 @pytest.fixture
-def data():
-    N = 100
-    k = 2
-    G= 20
-    X = np.random.normal(0, 1, N * k).reshape((N,k))
-    beta = np.random.normal(0,1,k)
-    beta[0] = 0.1
-    u = np.random.normal(0,1,N)
-    y = 1 + X @ beta + u
-    cluster = np.random.choice(list(range(0,G)), N)
-    bootcluster = cluster
-    R = np.zeros(k)
-    R[0] = 1
-    B = 999
-    
-    return X, y, cluster, bootcluster, R, B
+def data(G):
+  np.random.seed(12312)
+  N = 1000
+  k = 3
+  # small sample size -> full enumeration
+  X = np.random.normal(0, 1, N * k).reshape((N,k))
+  X[:,0] = 1
+  beta = np.random.normal(0,1,k)
+  beta[1] = 0.005
+  u = np.random.normal(0,1,N)
+  Y = X @ beta + u
+  cluster = np.random.choice(list(range(0,G)), N)
+  X_df = pd.DataFrame(X)
+  Y_df = pd.DataFrame(Y)
+  cluster_df = pd.DataFrame(cluster)
+  df = pd.concat([X_df, Y_df, cluster_df], axis = 1)  
+  df.columns = ['intercept','X1','X2','Y', 'cluster']
+
+  return df
 
 def test_results_from_same_seed():
     
-    df = pd.read_csv("https://raw.github.com/vincentarelbundock/Rdatasets/master/csv/sandwich/PetersenCL.csv")
-    df['treat'] = np.random.choice([0, 1], df.shape[0], True)
-    model = sm.ols(formula='y ~ treat', data=df)    
+    df = data(G = 20)
+    model = sm.ols(formula='Y ~ X1 + X2', data=df)    
 
     cluster_list = [df.years, None]:   
     for x in cluster_list: 
-        
+
         # same seed used in function -> same results
-        a = wildboottest(model, param = "treat", cluster = x, B= 999, seed=11232198237961)
-        b = wildboottest(model, param = "treat", cluster = x, B= 999, seed=11232198237961)
+        a = wildboottest(model, param = "X1", cluster = x, B= 999, seed=11232198237961)
+        b = wildboottest(model, param = "X1", cluster = x, B= 999, seed=11232198237961)
         pd.testing.assert_frame_equal(a,b)
 
         # random seed set outside of function and in function produce equal results
         np.random.default_rng(123)
-        a1 = wildboottest(model, param = "treat", cluster = x, B= 999)
-        b1 = wildboottest(model, param = "treat", cluster = x, B= 999, seed=123)
+        a1 = wildboottest(model, param = "X1", cluster = x, B= 999)
+        b1 = wildboottest(model, param = "X1", cluster = x, B= 999, seed=123)
         pd.testing.assert_frame_equal(a1,b1)
         
         # random seed outside of function 2x -> same results
         np.random.default_rng(123)
-        a2 = wildboottest(model, param = "treat", cluster = x, B= 999)
+        a2 = wildboottest(model, param = "X1", cluster = x, B= 999)
         np.random.default_rng(123)
-        b2 = wildboottest(model, param = "treat", cluster = x, B= 999)
+        b2 = wildboottest(model, param = "X1", cluster = x, B= 999)
         pd.testing.assert_frame_equal(a2,b2)
 
